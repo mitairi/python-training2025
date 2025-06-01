57b97029398261f8d8a9d87915e98e662be6a932
Irina Mitavskaya <imitavska@gmail.com>
2025-06-02 00:59:19 +0300

    Вынесла метод wd из тестов во вспомогательные функции

--- test_add_contact.py
+++ test_add_contact.py
@@ -12,38 +12,38 @@ class TestAddContact(unittest.TestCase):
     def setUp(self):
         self.wd = webdriver.Chrome()
         self.wd.implicitly_wait(30)
-        self.base_url = "https://www.google.com/"
-        self.verificationErrors = []
-        self.accept_next_alert = True
     
     def test_add_contact(self):
-        wd = self.wd
-        self.login(wd, username = "admin", password = "secret")
-        self.create_contact(wd, Contact(lastname = "last", firstname = "first"))
-        self.logout(wd)
+        self.login(username = "admin", password = "secret")
+        self.create_contact(Contact(lastname = "last", firstname = "first"))
+        self.logout()
 
-    def logout(self, wd):
+    def logout(self):
+        wd = self.wd
         wd.find_element(By.LINK_TEXT, "Logout").click()
 
-    def create_contact(self, wd, contact):
-        self.open_contacts_page(wd)
+    def create_contact(self, contact):
+        wd = self.wd
+        self.open_contacts_page()
         wd.find_element(By.NAME, "firstname").click()
         wd.find_element(By.NAME, "firstname").clear()
         wd.find_element(By.NAME, "firstname").send_keys(contact.firstname)
         wd.find_element(By.NAME, "lastname").clear()
         wd.find_element(By.NAME, "lastname").send_keys(contact.lastname)
         wd.find_element(By.NAME, "submit").click()
-        self.return_to_home_page(wd)
+        self.return_to_home_page()
 
-    def return_to_home_page(self, wd):
+    def return_to_home_page(self):
+        wd = self.wd
         wd.find_element(By.LINK_TEXT, "home page").click()
 
-    def open_contacts_page(self, wd):
+    def open_contacts_page(self):
+        wd = self.wd
         wd.find_element(By.LINK_TEXT, "add new").click()
 
-    def login(self, wd, username, password):
+    def login(self, username, password):
         wd = self.wd
-        wd.get("http://localhost/addressbook/")
+        self.open_home_page()
         wd.find_element(By.NAME, "user").click()
         wd.find_element(By.NAME, "user").clear()
         wd.find_element(By.NAME, "user").send_keys(username)
@@ -52,6 +52,10 @@ class TestAddContact(unittest.TestCase):
         wd.find_element(By.NAME, "pass").send_keys(password)
         wd.find_element(By.XPATH, "//input[@value='Login']").click()
         return wd
+
+    def open_home_page(self):
+        wd = self.wd
+        wd.get("http://localhost/addressbook/")
     
     def is_element_present(self, how, what):
         try: self.wd.find_element(by=how, value=what)
@@ -63,20 +67,8 @@ class TestAddContact(unittest.TestCase):
         except NoAlertPresentException as e: return False
         return True
     
-    def close_alert_and_get_its_text(self):
-        try:
-            alert = self.wd.switch_to.alert
-            alert_text = alert.text
-            if self.accept_next_alert:
-                alert.accept()
-            else:
-                alert.dismiss()
-            return alert_text
-        finally: self.accept_next_alert = True
-    
     def tearDown(self):
         self.wd.quit()
-        self.assertEqual([], self.verificationErrors)
 
 if __name__ == "__main__":
     unittest.main()

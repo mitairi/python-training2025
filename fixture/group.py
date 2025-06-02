9842d5ff7b8f91a4d8c95b3894fde896620fa7dc
Irina Mitavskaya <imitavska@gmail.com>
2025-06-03 00:51:10 +0300

    Задание 7. Добавлен тест  на модификацию группы

--- fixture/group.py
+++ fixture/group.py
@@ -15,6 +15,13 @@ class GroupHelper:
         # init group creation
         wd.find_element(By.NAME, "new").click()
         # fill in new group form
+        self.fill_in_group_info(group)
+        # click submit for group creation
+        wd.find_element(By.NAME, "submit").click()
+        self.return_to_groups_page()
+
+    def fill_in_group_info(self, group):
+        wd = self.app.wd
         wd.find_element(By.NAME, "group_name").click()
         wd.find_element(By.NAME, "group_name").clear()
         wd.find_element(By.NAME, "group_name").send_keys(group.name)
@@ -24,9 +31,6 @@ class GroupHelper:
         wd.find_element(By.NAME, "group_footer").click()
         wd.find_element(By.NAME, "group_footer").clear()
         wd.find_element(By.NAME, "group_footer").send_keys(group.footer)
-        # click submit for group creation
-        wd.find_element(By.NAME, "submit").click()
-        self.return_to_groups_page()
 
     def delete_first_group(self):
         wd = self.app.wd
@@ -36,6 +40,16 @@ class GroupHelper:
         # submit deletion
         wd.find_element(By.NAME, "delete").click()
         self.return_to_groups_page()
+
+    def modify_first_group(self, group):
+        wd = self.app.wd
+        self.open_groups_page()
+        # select first group
+        wd.find_element(By.NAME, "selected[]").click()
+        wd.find_element(By.NAME, "edit").click()
+        self.fill_in_group_info(group)
+        wd.find_element(By.NAME, "update").click()
+        self.return_to_groups_page()    
 
     def return_to_groups_page(self):
         wd = self.app.wd

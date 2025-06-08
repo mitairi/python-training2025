9208d854d0ce8c598e4afaa415d5c22624a42c4e
Irina Mitavskaya <imitavska@gmail.com>
2025-06-08 15:39:02 +0300

    Добавлены предусловия для модификации групп

--- test/test_del_group.py
+++ test/test_del_group.py
@@ -4,5 +4,5 @@ from model.group import Group
 
 def test_delete_first_group(app):
     if app.group.count() == 0:
-        app.group.create(name="del_test")
+        app.group.create(Group(name="del_test"))
     app.group.delete_first_group()
--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -2,13 +2,21 @@
 from model.group import Group
 
 def test_modify_first_group(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="del_test"))
     app.group.modify_first_group(Group(name = "NewName", header = "NewHeader", footer = "NewFooter"))
 
 def test_modify_first_group_name(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="del_test"))
     app.group.modify_first_group(Group(name = "New Name"))
 
 def test_modify_first_group_footer(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="del_test"))
     app.group.modify_first_group(Group(footer = "New Footer"))
 
 def test_modify_first_group_header(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="del_test"))
     app.group.modify_first_group(Group(header = "New Header"))
